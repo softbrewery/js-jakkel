35deb7a8c1269cb07135c1ad558dc95067c1abed
Brian Baker <brian_e_baker@yahoo.com>
2014-12-16 11:43:41 +0100

    updated functionality

--- src/jakkel.js
+++ src/jakkel.js
@@ -18,15 +18,65 @@
    */
   Jakkel.prototype.VERSION = '0.0.2';
 
+   /* Function:
+    * Flush the config, used for testing. 
+    */
+   var Jakkal.prototype.flush = function() {
+     this._conifig.roles = [];
+     this._conifig.resources = [];
+   };
+
+   /* Function:
+    * Get roles
+    * @return Array
+    */
+    Jakkel.prototype.roles = function() {
+      return this._config.roles;
+    };
+
+   /* Function:
+    * Get Role
+    * @param roleName
+    * @return Object
+    */
+  Jakkel.prototype.role = function(roleName) {
+    for(var r in this._config.roles) {
+      if (r.role === roleName ) {
+        return r;
+      }
+    }
+    return;
+  }
+  
+  /* Function:
+   * Add role
+   * @param role string
+   * @param parent string
+   * @return true on success
+   */
+  Jakkel.prototype.addRole = function(roleName, parent) {
+    if ( roleName === null || typeof roleName !== 'string' ||
+         roleName.length() === 0 || this.role(roleName)) {
+      return false;
+    }
+    var new_role = {};
+    new_role.role = roleName;
+    new_role.parent = parent;
+    this._config.roles.push(new_role);
+    return true;
+  }
+
 
   //##############################################################
   
   /**
    * Clear the contents of an Array
    */
-  Array.prototype.clear = function() {
-    while (this.length > 0) {
-      this.pop();
+  if ( typeof Array.clear !== 'undefined' {
+    Array.prototype.clear = function() {
+      while (this.length > 0) {
+        this.pop();
+      }
     }
   };
 
@@ -36,16 +86,13 @@
    * Export Jakkel for node.js and browsers
    */
   if ( typeof module === "object" && module && typeof module.exports === "object" && exports ) {
-    
-      module.exports = Jakkel;
+    module.exports = Jakkel;
   } 
   else if(window) {
-
-      window.Jakkel = Jakkel;
-
-      if ( typeof define === "function" && define.amd ) {
-          define( "Jakkel", [], function () { return Jakkel; } );
-      }
+    window.Jakkel = Jakkel;
+    if ( typeof define === "function" && define.amd ) {
+      define( "Jakkel", [], function () { return Jakkel; } );
+    }
   }
 
 })(this);
--- test/jakkel.spec.js
+++ test/jakkel.spec.js
@@ -1,3 +1,24 @@
-describe("Jakkel", function() {
+ //var Jakkel = require('../src/jakkel.js');
+//var should = require('should');
 
+describe("Jakkel tests", function() {
+  var jakkel = new Jakkel;
+
+  beforeEach(function() {
+    jakkel.flush();
+  });
+
+  describe("version property", function() {
+    it("should be defined", function() {
+      expect(jakkel.VERSION).to.be.a('string');
+    });
+    it("should not be empty", function() {
+      expect(jakkel.VERSION.length()).to.be.greaterThan(4)
+    });
+  });
+    /*
+    describe("roles() method", function() {
+        it("should be define", function() {
+            jakkel.should.have.
+    */
 });
