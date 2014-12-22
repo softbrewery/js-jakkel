151dc3d92976b099255a96777b33a2d1e449d68e
Brian Baker <brian_e_baker@yahoo.com>
2014-12-22 17:34:33 +0100

    added coverage

--- karma.conf.js
+++ karma.conf.js
@@ -28,14 +28,25 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      '**/src/*.js': 'coverage'
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['mocha'],
+    reporters: [ 'mocha', 'coverage'],
 
+    coverageReporter: {
+      //type : 'text',
+      dir : 'coverage/',
+      //file : 'coverage.txt'
+      reporters: [ 
+          { type: 'lcov', subdir: 'lcov' },
+          { type: 'html', subdir: 'html' },
+          { type: 'text', file: 'coverage.txt' }
+      ]
+    },
 
     // web server port
     port: 9876,
